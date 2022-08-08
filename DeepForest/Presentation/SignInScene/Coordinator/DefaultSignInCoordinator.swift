59e0956d577d7df7c3ed6b7ed44938c8b648f9a0
wolframhwang <hzw94@naver.com>
2022-08-09 01:20:45 +0900

    [feat] SignInScene Coordinator finish 함수 구현

--- DeepForest/Presentation/SignInScene/Coordinator/DefaultSignInCoordinator.swift
+++ DeepForest/Presentation/SignInScene/Coordinator/DefaultSignInCoordinator.swift
@@ -27,4 +27,8 @@ class DefaultSignInCoordinator: SignInCoordinator {
     func pushSignInViewController() {
         self.navigationController.pushViewController(self.signInViewController, animated: true)
     }
+    
+    func finish() {
+        self.finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
 }
