86519364e018e283e21f7f8b24ae97577c69bab0
wolframhwang <hzw94@naver.com>
2022-08-14 00:35:57 +0900

    [feat] Network: DataMapping: SignUpResponseDTO 추가

---
+++ DeepForest/Network/DataMapping/SignUpResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpResponseDTO.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/14.
+//
+
+import Foundation
+
+struct SignUpResponseDTO: Codable {
+    let success: Bool
+    let error: RESTError?
+}
