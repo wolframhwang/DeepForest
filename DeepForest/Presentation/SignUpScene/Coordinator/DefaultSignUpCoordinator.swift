b59b768b3fc5e8574b75b4d45c760ba1be2f4d5e
wolframhwang <hzw94@naver.com>
2022-08-13 23:36:29 +0900

    [feat] SignUpScene: SignUpScene Coordinator 구현 사항 반영

--- DeepForest/Presentation/SignUpScene/Coordinator/DefaultSignUpCoordinator.swift
+++ DeepForest/Presentation/SignUpScene/Coordinator/DefaultSignUpCoordinator.swift
@@ -6,7 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
 final class DefaultSignUpCoordinator: SignUpCoordinator {
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var signUpViewController: SignUpViewController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .signup
     
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.signUpViewController = SignUpViewController()
+    }
+    
+    func start() {
+        
+    }
+    
+    func pushSignUpViewController() {
+        signUpViewController.viewModel = SignUpViewModel(coordinator: self,
+                                                              signUpUseCase: DefaultSignUpUseCase(networkRepository: DefaultNetworkRepository(network: DefaultURLSessionNetworkService()
+                                                                )
+                                                            )
+                                                )
+        
+        self.navigationController.pushViewController(signUpViewController, animated: true)
+    }
+    
+    func finish() {
+        self.finishDelegate?.coordinatorDidFinish(childCoordinator: self)
+    }
 }
