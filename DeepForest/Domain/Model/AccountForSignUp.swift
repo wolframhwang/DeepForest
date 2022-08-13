582a0871f917720d8845882c817a02b1ebf80bb9
wolframhwang <hzw94@naver.com>
2022-08-14 00:15:40 +0900

    [feat] Domain: Model: SignUpAccount Model 구현

--- DeepForest/Domain/Model/AccountForSignUp.swift
+++ DeepForest/Domain/Model/AccountForSignUp.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 struct AccoutForSignUp: Codable {
+    let userId: String
+    let password: String
+    let email: String
+    let nickName: String
     
+    private enum CodingKeys: String, CodingKey {
+        case userId = "username"
+        case password, email
+        case nickName = "nickname"
+    }
 }
