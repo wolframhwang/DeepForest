0985a3594cce3fafd805cdea7703ff1278327014
wolframhwang <hzw94@naver.com>
2022-08-13 20:16:08 +0900

    [feat] URLSessionNetworkServiceError 에러 메세지 처리를 위한 부분 추가

--- DeepForest/Network/DefaultURLSessionNetworkServiceError.swift
+++ DeepForest/Network/DefaultURLSessionNetworkServiceError.swift
@@ -13,3 +13,16 @@ enum URLSessionNetworkServiceError: Int, Error {
     case unknownError
     case emptyDataError
 }
+
+extension URLSessionNetworkServiceError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .emptyDataError:
+            return "비어있는 데이터"
+        case .invalidURLError:
+            return "잘못된 주소"
+        case .unknownError:
+            return "이유 불명의 데이터"
+        }
+    }
+}
