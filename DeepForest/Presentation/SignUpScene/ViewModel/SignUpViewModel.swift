a43203112c03e454e5c793e8b35e388b9330742d
wolframhwang <hzw94@naver.com>
2022-08-13 22:53:59 +0900

    [feat] SignUp: SignUp ViewModel Input Modeling

--- DeepForest/Presentation/SignUpScene/ViewModel/SignUpViewModel.swift
+++ DeepForest/Presentation/SignUpScene/ViewModel/SignUpViewModel.swift
@@ -10,15 +10,35 @@ import RxSwift
 import RxCocoa
 
 final class SignUpViewModel: ViewModelType {
+    private weak var coordinator: SignUpCoordinator?
+    private let signUpUseCase: SignUpUseCase
+    
+    private let disposeBag = DisposeBag()
+    
     struct Input {
-        
+        let idTextFieldDidEditEvent: Observable<String>
+        let nickNameTextFieldDidEditEvent: Observable<String>
+        let pwTextFieldDidEditEvent: Observable<String>
+        let repwTextFieldDidEditEvent: Observable<String>
+        let emailTextFieldDidEditEvent: Observable<String>
+        let submitButtnTapped: Observable<Void>
     }
     
     struct Output {
         
     }
     
+    init(coordinator: SignUpCoordinator?,
+         signUpUseCase: SignUpUseCase) {
+        self.coordinator = coordinator
+        self.signUpUseCase = signUpUseCase
+    }
+    
     func transform(from input: Input) -> Output {
         return Output()
+    }
+    
+    private func configureInput(_ input: Input) {
+        
     }
 }
