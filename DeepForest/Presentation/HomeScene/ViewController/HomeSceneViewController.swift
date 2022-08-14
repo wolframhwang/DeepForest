7d2c19033e12b5cd073f47d591b656ea8dd6e569
wolframhwang <hzw94@naver.com>
2022-08-14 20:57:32 +0900

    [feat] HomeScene: Scene 기본 Frame반영

--- DeepForest/Presentation/HomeScene/ViewController/HomeSceneViewController.swift
+++ DeepForest/Presentation/HomeScene/ViewController/HomeSceneViewController.swift
@@ -13,8 +13,53 @@ import SnapKit
 class HomeSceneViewController: UIViewController {
     private let disposeBag = DisposeBag()
     var viewModel: HomeSceneViewModel?
+    
+    private lazy var sideMenuButton: UIBarButtonItem = {
+        let menuButton = UIBarButtonItem()
+        menuButton.style = .done
+        menuButton.image = UIImage(systemName: "line.horizontal.3")
+        
+        return menuButton
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        configureSubviews()
+        configureUI()
+        configureTableView()
+        bindViewModel()
+    }
+    
+    private func configureTableView() {
+        tableView.refreshControl = UIRefreshControl()
+        tableView.estimatedRowHeight = 64
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    
+}
+
+extension HomeSceneViewController {
+    func configureSubviews() {
+        navigationItem.leftBarButtonItem = sideMenuButton
+        
+        view.addSubview(tableView)
+    }
+    
+    func configureUI() {
+        title = "DEEP FOREST"
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func bindViewModel() {
+        
     }
 }
