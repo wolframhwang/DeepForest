b3c3887093a0dc3c406d13fb1884ba57d7636123
wolframhwang <hzw94@naver.com>
2022-08-15 00:31:34 +0900

    [feat] MenuScene: MenuItem 추가 반영

---
+++ DeepForest/Domain/Model/MenuItem.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItem.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/15.
+//
+
+import Foundation
+
+enum MenuItems {
+    case galleryList
+}
+
+struct MenuItem {
+    let title: String
+    let item: MenuItems
+}
