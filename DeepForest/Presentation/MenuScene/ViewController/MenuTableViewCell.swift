12fca08588e5dc7cf1ec061f40625fd6d5750121
wolframhwang <hzw94@naver.com>
2022-08-15 01:14:31 +0900

    [feat] MenuTrableViewCell: Cell 뷰 초기화 단계 반영

--- DeepForest/Presentation/MenuScene/ViewController/MenuTableViewCell.swift
+++ DeepForest/Presentation/MenuScene/ViewController/MenuTableViewCell.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
 class MenuTableViewCell: UITableViewCell {
     private lazy var titleLabel = UILabel()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.trailing.leading.equalToSuperview().inset(10)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Cell Error")
     }
     
     func bind(_ viewModel: MenuTableCellViewModel) {
         self.titleLabel.text = viewModel.title
+        print("BIND", viewModel.title)
     }
 }
