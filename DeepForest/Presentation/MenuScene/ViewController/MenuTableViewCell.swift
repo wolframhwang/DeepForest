b75dfd02b66b16551f310cab99194cb8caa30535
wolframhwang <hzw94@naver.com>
2022-08-15 00:47:07 +0900

    [feat] MenuTableViewCell 추가 부분 구현 사항 반영

--- DeepForest/Presentation/MenuScene/ViewController/MenuTableViewCell.swift
+++ DeepForest/Presentation/MenuScene/ViewController/MenuTableViewCell.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class MenuTableViewCell: UITableViewCell {
-
+    private lazy var titleLabel = UILabel()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func bind(_ viewModel: MenuTableCellViewModel) {
+        self.titleLabel.text = viewModel.title
     }
-
 }
