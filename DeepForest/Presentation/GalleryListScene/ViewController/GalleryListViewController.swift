9adf958110a6a224f2be3ed48e8d4b772c3a3036
wolframhwang <hzw94@naver.com>
2022-08-15 17:53:22 +0900

    [feat] GalleryListScene: tableview 생성

--- DeepForest/Presentation/GalleryListScene/ViewController/GalleryListViewController.swift
+++ DeepForest/Presentation/GalleryListScene/ViewController/GalleryListViewController.swift
@@ -7,14 +7,41 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
+import SnapKit
 
 class GalleryListViewController: UIViewController {
     var viewModel: GalleryListViewModel?
     private let disposeBag = DisposeBag()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Test Code
         view.backgroundColor = .red
+        configureSubviews()
+        configureUI()
+        setAttribute()
+        bindViewModel()
+    }
+}
+
+extension GalleryListViewController {
+    func configureSubviews() {
+        
+    }
+    func configureUI() {
+        
+    }
+    func setAttribute() {
+        
+    }
+    func bindViewModel() {
+        
     }
 }
