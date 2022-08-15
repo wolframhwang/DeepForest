72f49f94332b66638e67e610fa8f61d938aaf390
wolframhwang <hzw94@naver.com>
2022-08-15 17:59:40 +0900

    [feat] GalleryListScene: GalleryListScene UseCase 생성

---
+++ DeepForest/Domain/UseCase/GalleryList/DefaultGalleryListUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultGalleryListUseCase.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/15.
+//
+
+import Foundation
+import RxSwift
+
+final class DefaultGalleryListUseCase: GalleryListUseCase {
+    
+}
---
+++ DeepForest/Domain/UseCase/GalleryList/Protocol/GalleryListUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  GalleryListUseCase.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/15.
+//
+
+import Foundation
+import RxSwift
+
+protocol GalleryListUseCase {
+    
+}
