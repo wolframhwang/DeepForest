80f6acc2543b9d4684fb5bc1606f4964655894d0
wolframhwang <hzw94@naver.com>
2022-08-15 12:09:48 +0900

    [feat] Util: Constant: UserDefaultKey 반영

---
+++ DeepForest/Util/Constant/UserDefaultsKey.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultsKey.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/15.
+//
+
+import Foundation
+
+enum UserDefaultsKey {
+    static let nickname = "nickname"
+    static let token = "token"
+    static let refreshToken = "refreshToken"
+}
