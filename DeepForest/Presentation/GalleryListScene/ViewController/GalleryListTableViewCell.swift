21c069c690f5ce728929c7debdde4502965c3e56
wolframhwang <hzw94@naver.com>
2022-08-15 17:56:17 +0900

    [feat] GalleryListScene: GalleryList TableviewCell 추가

---
+++ DeepForest/Presentation/GalleryListScene/ViewController/GalleryListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GalleryListTableViewCell.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/15.
+//
+
+import UIKit
+
+class GalleryListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DeepForest/Presentation/GalleryListScene/ViewModel/GalleryListCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  GalleryListTableViewCell.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/15.
+//
+
+import Foundation
+
+final class GalleryListCellViewModel {
+    
+}
