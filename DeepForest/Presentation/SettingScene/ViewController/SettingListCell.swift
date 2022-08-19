9834b4567c6e419c7700e89961ae964750aaa403
wolframhwang <hzw94@naver.com>
2022-08-19 23:08:02 +0900

    [feat] SettingScene: SettingCell 추가

---
+++ DeepForest/Presentation/SettingScene/ViewController/SettingListCell.swift
@@ -0,0 +1,21 @@
+//
+//  SettingListCell.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/19.
+//
+
+import UIKit
+
+class SettingListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
