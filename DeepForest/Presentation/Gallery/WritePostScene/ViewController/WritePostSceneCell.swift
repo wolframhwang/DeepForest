79f89a825dba15eb4c6ee2a820f46e0abf07746b
wolframhwang <hzw94@naver.com>
2022-08-28 14:11:52 +0900

    [feat] WritePostScene: Cell 추가사항 반영

---
+++ DeepForest/Presentation/Gallery/WritePostScene/ViewController/WritePostSceneCell.swift
@@ -0,0 +1,26 @@
+//
+//  WritePostSceneCell.swift
+//  DeepForest
+//
+//  Created by 황지웅 on 2022/08/28.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+class WritePostSceneCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
